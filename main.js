82970ba41cca3aa125d1831042998ad928c9891d
ehisgodswill <ehisgodswill@gmail.com>
2022-11-09 19:09:38 +0100

    removed transition

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ let num = document.getElementById('num')
 let n = 0
 function showSlide() {
     const items = document.getElementsByClassName('carouselitem');
-    carousel.style.transition = '0.3s ease'
+    // carousel.style.transition = '0.3s ease'
     if (n > items.length - 1) {
         n = 0
     } else if (n < 0) {
         n = items.length - 1
     } else {
-        carousel.style.transition = '1s ease'
+        // carousel.style.transition = '1s ease'
     } Array(...items).forEach((element, index) => {
         num.innerHTML = `${n+1}/${items.length}`
         element.style.transform = `translate(${-500 * n}px)`
